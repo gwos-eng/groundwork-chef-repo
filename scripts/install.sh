5c4a34ea23eb27149804489bcaef8b61c2cb650a
Miguel <miguel.deelias@gmail.com>
2013-01-09 17:38:06 +0000

    adding date and time to node name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
-sudo knife ec2 server create -I ami-3d4ff254 -f m1.small -x ubuntu --region 'us-east-1' -Z 'us-east-1a' -G quick-start-1 -N groundworkos -i ../.chef/demo.pem -S demo -r 'recipe[groundwork::before_install]'
-sudo knife ssh name:groundworkos -x ubuntu -i ../.chef/demo.pem "sudo chef-client && sudo reboot"
-sleep 180
-knife node run_list remove groundworkos 'recipe[groundwork::before_install]'
-knife node run_list add groundworkos 'recipe[groundwork]'
-sudo knife ssh name:groundworkos -x ubuntu -i ../.chef/demo.pem "sudo chef-client"
+nodename=groundwork_$(date +"%d_%m_%Y-%H.%M.%S")
+sudo knife ec2 server create -I ami-3d4ff254 -f m1.small -x ubuntu --region 'us-east-1' -Z 'us-east-1a' -G quick-start-1 -N $nodename -i ../.chef/demo.pem -S demo -r 'recipe[groundwork::before_install]'
+sudo knife ssh name:$nodename -x ubuntu -i ../.chef/demo.pem "sudo chef-client && sudo reboot"
+sleep 220
+knife node run_list remove $nodename 'recipe[groundwork::before_install]'
+knife node run_list add $nodename 'recipe[groundwork]'
+sudo knife ssh name:$nodename -x ubuntu -i ../.chef/demo.pem "sudo chef-client"
